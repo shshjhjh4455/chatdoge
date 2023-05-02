3e150e686f712b6cef77f67e35a95bb3e99c9797
jeonghyeonPark <shshjhjh4455@gmail.com>
2023-05-02 10:47:29 +0900

    백엔드 서버 구성 테스트

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,21 @@
-const apiKey = "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";
+require('dotenv').config();
+
+const apiKey = process.env.API_KEY;
 const { Configuration, OpenAIApi } = require("openai");
+
+const express = require('express')
+const app = express()
+
+let corsOptions =
+
+    app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// POST method route
+app.post('/', function (req, res) {
+    res.send('POST request to the homepage')
+})
+app.listen(3000)
 
 const configuration = new Configuration({
     apiKey: apiKey,
@@ -12,7 +28,8 @@ async function apiCall() {
         messages: [{ role: "system", content: "hello" },
         { role: "user", content: "Hi, how are you?" },
         { role: "assistant", content: "I'm good, how are you?" },
-        { role: "user", content: "I'm good, thanks!" },],
+        { role: "user", content: "I'm good, thanks!" },
+        ],
     });
     console.log(completion.data.choices[0].messages);
 }
